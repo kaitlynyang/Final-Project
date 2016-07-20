31fadb9fbb78a6c04761cb4ff23615aaf9c4b1a1
kaitlynyang <kaitlynyang2010@gmail.com>
2016-07-20 10:50:12 -0700

    added location

--- js/finalproject.js
+++ js/finalproject.js
@@ -1,7 +1,7 @@
 function initMap() {
         var mapDiv = document.getElementById('map');
         var map = new google.maps.Map(mapDiv, {
-            center: {lat: 44.540, lng: -78.546},
-            zoom: 8
+            center: {lat: 33.9301882, lng: -118.38374570000002},
+            zoom: 10
         });
       }
