fc29b73145fd47701d8bc03bf457042c8a7daae4
kaitlynyang <kaitlynyang2010@gmail.com>
2016-07-20 14:18:04 -0700

    map test

--- js/finalproject.js
+++ js/finalproject.js
@@ -1,7 +1,73 @@
+//Attempt 1 at location//
+// function getLocation() {
+// 	console.log("got location");
+// 	var address = document.getElementById("address1");
+//   	var geocoder = new google.maps.Geocoder();
+// 	geocoder.geocode( { 'address': address}, function(results, status) {
+
+// 	if (status == google.maps.GeocoderStatus.OK) {
+// 	   	var latitude = results[0].geometry.location.lat();
+// 	    var longitude = results[0].geometry.location.lng();
+// 	    console.log(latitude, longitude);
+// 	    }
+// 	});
+// }
+
+//Attempt 2 at location//
+
 function initMap() {
-        var mapDiv = document.getElementById('map');
-        var map = new google.maps.Map(mapDiv, {
-            center: {lat: 33.9301882, lng: -118.38374570000002},
-            zoom: 10
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13
+        });
+
+        var input = document.getElementById('pac-input');
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
+
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+          map: map
+        });
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            return;
+          }
+
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+          }
+
+          // Set the position of the marker using the place ID and location.
+          marker.setPlace({
+            placeId: place.place_id,
+            location: place.geometry.location
+          });
+          marker.setVisible(true);
+
+          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+              'Place ID: ' + place.place_id + '<br>' +
+              place.formatted_address);
+          infowindow.open(map, marker);
         });
       }
+
+// function initMap() {
+//     var mapDiv = document.getElementById("map");
+//     var map = new google.maps.Map(mapDiv, {
+//         center: {lat: -100, lng: 100},
+//         zoom: 10
+//     });
+//}
