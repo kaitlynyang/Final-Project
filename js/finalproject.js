6fff75585cbb91b1d5e3290a3b9b419bc2f0e0cc
Nichole <nicholexsun@gmail.com>
2016-07-21 13:46:04 -0700

    added tons more events

--- js/finalproject.js
+++ js/finalproject.js
@@ -3,32 +3,64 @@ function initMap() {
     	center:  {lat:21.007890, lng: -10.940835},
         zoom: 1
     });
-
-        var marker1 = new google.maps.Marker({
+// Environment Events
+        var enviro1 = new google.maps.Marker({
         	position: {lat:53.955820, lng: -1.079940},
         	map: Newmap,
         	title: "No Fracking Demonstration\nJuly 30, 2016\n12:00pm to 2:30pm\nCliffords Tower, Yorkshire, England",
           icon: "images/greendot.jpg"
         });
 
-    		var marker2 = new google.maps.Marker({
+        var enviro2 = new google.maps.Marker({
+          position: {lat:33.839408, lng: -118.390386},
+          map: Newmap,
+          title: "Nothin' But Sand Beach Cleanup\nAugust 20, 2016\n10:00am to 12:00pm\nRedondo Beach Pier, Redondo Beach, California",
+          icon: "images/greendot.jpg"
+        });
+
+// Feminism Events
+    		var feminism1 = new google.maps.Marker({
     			position: {lat:38.907192, lng:-77.036871},
     		  map: Newmap,
     		  title: "National Sexual Assault Conference 2016\nAugust 31, 2016\nAll day\n2660 Woodley Road NW, Washinton, DC",
           icon: "images/reddot.jpg"
     		});
 
-        var marker3 = new google.maps.Marker({
+        var feminism2 = new google.maps.Marker({
+          position: {lat:-34.630629, lng:-58.427403},
+          map: Newmap,
+          title: "Association for Women's Rights in Development International Forum\nSeptember 8-11, 2016\nCosta do Sauipe, Bahia, Brazil",
+          icon: "images/reddot.jpg"
+        });
+
+
+// Human Rights Events
+        var humanrights1 = new google.maps.Marker({
             position: {lat:40.820897, lng:-73.953896},
             map: Newmap,
             title: "The People’s Resistance to the Venezuelan Food Crisis\nJuly 30, 2016\n4:00pm to 6:00pm\n9A Hamilton Place, Boston, Massachusetts",
             icon: "images/yellowdot.jpg"
         });
 
-        var marker4 = new google.maps.Marker({
+        var humanrights2 = new google.maps.Marker({
+            position: {lat:34.274531, lng:-119.300538},
+            map: Newmap,
+            title: "Ventura County Pride\nAugust 20, 2016\n11:00am - 6:00pm\nPromenade Park, Ventura, California",
+            icon: "images/yellowdot.jpg"
+        });
+
+// Education Events
+        var education1 = new google.maps.Marker({
             position: {lat:46.905900, lng:-95.058900},
             map: Newmap,
             title: "Educational Equity in Action\nJune 20 & 21, 2016\nUniversity of Minnesota West Bank Campus",
+            icon: "images/bluedot.jpg"
+        });
+
+        var education2 = new google.maps.Marker({
+            position: {lat:37.774929, lng:-122.419416},
+            map: Newmap,
+            title: "Summit on Improvement in Education\nMarch 27-29, 2017\nSan Francisco Marriott Marquis",
             icon: "images/bluedot.jpg"
         });
 
