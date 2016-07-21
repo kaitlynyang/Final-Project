53a6cf571080e0d6eb6792a7cfaacfed7dbf70ed
kaitlynyang <kaitlynyang2010@gmail.com>
2016-07-21 09:09:25 -0700

    changed js

--- js/finalproject.js
+++ js/finalproject.js
@@ -16,52 +16,30 @@
 //Attempt 2 at location//
 
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-    	center: {lat: -33.8688, lng: 151.2195},
-        zoom: 13
+    var Newmap = new google.maps.Map(document.getElementById('map'), {
+    	center:  {lat:-25.274, lng: 133.775},
+        zoom: 1
     });
 
-    var input = document.getElementById('pac-input');
-
-        var autocomplete = new google.maps.places.Autocomplete(input);
-        autocomplete.bindTo('bounds', map);
-
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-        var infowindow = new google.maps.InfoWindow();
-        var marker = new google.maps.Marker({
-          map: map
-        });
-        marker.addListener('click', function() {
-          infowindow.open(map, marker);
+        var marker1 = new google.maps.Marker({
+        	position: {lat:-25.274, lng: 133.775},
+        	map: Newmap,
+        	title: "Australia"
         });
 
-        autocomplete.addListener('place_changed', function() {
-          infowindow.close();
-          var place = autocomplete.getPlace();
-          if (!place.geometry) {
-            return;
-          }
+		var marker2 = new google.maps.Marker({
+			position: {lat:-40.9, lng:174.88},
+		    map: Newmap,
+		    title: "New Zealand"
+		});
 
-          if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-          } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);
-          }
-
-          // Set the position of the marker using the place ID and location.
-          marker.setPlace({
-            placeId: place.place_id,
-            location: place.geometry.location
-          });
-          marker.setVisible(true);
-
-          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-              'Place ID: ' + place.place_id + '<br>' +
-              place.formatted_address);
-          infowindow.open(map, marker);
+        var marker3 = new google.maps.Marker({
+            position: {lat:33.93, lng:-118.38},
+            map: Newmap,
+            title: "New Zealand"
         });
+
+
 }
 
 // function initMap() {
